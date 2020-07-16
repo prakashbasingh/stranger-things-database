aed4a0656c8314821ce618342c65ab82b2853567
unknown <prakashbahadursingh2@gmail.com>
2020-07-16 16:23:57 -0700

    created migration

---
+++ database/migrations/20200716155747_00_Characters.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable("characters", tbl => {
+      tbl.increments()
+
+      tbl.string("name", 128)
+         .notNullable()
+         .unique()
+
+      tbl.string("gender", 32)
+         .notNullable()
+
+      tbl.string("imageurl", 128)
+         .notNullable()
+  })
+};
+
+exports.down = function(knex) {
+  
+};
