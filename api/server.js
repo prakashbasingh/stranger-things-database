74f8f9d70d18852480fd1b748f5c0bb1a6658a3f
unknown <prakashbahadursingh2@gmail.com>
2020-07-16 14:50:32 -0700

    end point / set up complete and server is running now

--- api/server.js
+++ api/server.js
@@ -8,5 +8,17 @@ const server = express();
 server.use(helmet())
 server.use(express.json());
 server.use(morgan("dev"))
+server.use(cors())
+
+server.get("/", (req, res) => {
+    // res.send("This is group project with out TL for backend week 2 sprint server is running")
+
+    data = {
+        message: "server is running /",
+        reply: "way too cool tool",
+    }
+    res.send(`${data.message} and ${data.reply}`)
+})
+
 
 module.exports = server
